cc7257f186b18fd6d3f267df0004289c54df10a0
Jonathan charlet <jonathan.charlet.pro@gmail.com>
2022-06-21 08:55:58 +0200

    welcome page finished and main page started

---
+++ Wefashion/app/Http/Controllers/ProductController.php
@@ -0,0 +1,15 @@
+<?php
+ 
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\User;
+ 
+class ProductController{
+    public function index()
+    {
+        $data= user::latest()->first();
+        return view('index'); // next:- page name created with next.blade.php 
+    }
+}
+
+
---
+++ Wefashion/resources/views/index.blade.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="../style/style.css">
+
+
+</head>
+<body>
+    <header class="br col-lg-12">
+        <nav class="navbar navbar-expand-lg bg-dark">
+            <div class="br col-lg-11 m-auto container-fluid">
+                <div class="col-lg-9">
+                    <a class="br font-monospace Title navbar-brand" href="#">WE FASHION</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </div>
+                
+                <div class="br col-lg-2 collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="br m-auto navbar-nav">
+                        <a class=" MenuText font-monospace nav-link" href="#">SOLDES</a>
+                        <a class=" MenuText font-monospace nav-link" href="#">HOMME</a>
+                        <a class=" MenuText font-monospace nav-link" href="#">FEMME</a>                    
+                    </div>
+                </div>
+            </div>
+          </nav>
+    </header>
+
+    <main class="br my-5 py-5 m-auto">
+        <div class="col-lg-12">
+
+        </div>
+    </main>
+
+    <footer class="br col-lg-12 text-center">
+        <div class="br col-lg-8 row m-auto">
+
+            <div class="br col-lg-4  ">
+                <h1 class="FooterTitle">Information</h1>
+                <div class="br col-lg-2  m-auto">
+                        <a class="br col-lg-12 FooterText font-monospace nav-link" href="#">SOLDES</a>
+                        <a class="br col-lg-12 FooterText font-monospace nav-link" href="#">SOLDES</a>
+                        <a class="br col-lg-12 FooterText font-monospace nav-link" href="#">SOLDES</a>
+                </div>
+                
+            </div>
+            <div class="br col-lg-4  ">
+                <h1 class="FooterTitle">Information</h1>
+                <div class="br col-lg-2  m-auto">
+                        <a class="br col-lg-12 FooterText font-monospace nav-link" href="#">SOLDES</a>
+                        <a class="br col-lg-12 FooterText font-monospace nav-link" href="#">SOLDES</a>
+                        <a class="br col-lg-12 FooterText font-monospace nav-link" href="#">SOLDES</a>
+                </div>
+                
+            </div>
+            <div class="br col-lg-4  ">
+                <h1 class="FooterTitle">Information</h1>
+                <div class="br col-lg-2  m-auto">
+                        <a class="br col-lg-12 FooterText font-monospace nav-link" href="#">SOLDES</a>
+                        <a class="br col-lg-12 FooterText font-monospace nav-link" href="#">SOLDES</a>
+                        <a class="br col-lg-12 FooterText font-monospace nav-link" href="#">SOLDES</a>
+                </div>
+                
+            </div>
+           
+
+        </div>
+
+        <div>
+
+        </div>
+
+        <div>
+
+        </div>
+
+
+    </footer>
+</body>
+</html>
